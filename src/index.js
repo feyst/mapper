0c4e6fd5bd05926379ea96dacfd578ba04eb3d59
Mark van der Feijst <mark@feyst.nl>
2021-07-04 22:46:56 +0200

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,323 @@
+import $ from 'jquery'
+import _ from 'underscore'
+import CodeMirror from 'codemirror/lib/codemirror'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/addon/mode/simple'
+import 'jshint/dist/jshint'
+import jsonlint from 'jsonlint/web/jsonlint'
+import 'codemirror/addon/lint/lint'
+import 'codemirror/addon/hint/javascript-hint'
+import 'codemirror/addon/lint/javascript-lint'
+import 'codemirror/addon/lint/html-lint'
+import 'codemirror/addon/lint/json-lint'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/fold/xml-fold'
+import 'codemirror/addon/fold/foldcode'
+import 'codemirror/addon/fold/foldgutter'
+import 'codemirror/addon/edit/matchtags'
+import 'codemirror/addon/edit/matchbrackets'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/scroll/simplescrollbars'
+import vkbeautify from 'vkbeautify'
+import axios from 'axios'
+
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/darcula.css'
+import 'codemirror/addon/lint/lint.css'
+import 'codemirror/addon/scroll/simplescrollbars.css'
+import './style.css'
+
+window.jsonlint = jsonlint
+
+const debounce = 600;
+
+const jqMode = {
+    start: [
+        {regex: / ?/, next: 'expression'}
+    ],
+    object: [
+        {regex: / ?/, token: 'bracket', push: 'key'},
+        {regex: ',', token: 'bracket', push: 'key'},
+        {regex: '}', token: 'bracket', pop: true}
+    ],
+    array: [
+        {regex: ',', token: 'bracket'},
+        {regex: / ?/, push: 'value'},
+        {regex: ']', token: 'bracket', pop: true}
+    ],
+    key: [
+        {regex: /[a-z]\w*/i, token: 'keyword'},
+        {regex: '"', token: 'string', push: 'string'},
+        {regex: /\(/, token: 'variable-2', push: 'expression'},
+        {regex: ':', token: 'bracket', push: 'value'}
+    ],
+    value: [
+        {regex: /null|true|false/, token: 'atom'},
+        {regex: /\d+/, token: 'number'},
+        {regex: /\.\w+/, token: 'tag'},
+        {regex: /\(/, token: 'variable-2', push: 'expression'},
+        {regex: '"', token: 'string', push: 'string'},
+        {regex: '{', token: 'bracket', push: 'object'},
+        {regex: /\[/, token: 'bracket', push: 'array'},
+        {regex: /[,}\]]/, token: 'bracket', pop: true}
+    ],
+    string: [
+        {regex: /[^"\\]+/, token: 'string'},
+        {regex: /\\\(/, token: 'variable-2', push: 'expression'},
+        {regex: '"', token: 'string', pop: true}
+    ],
+    expression: [
+        {regex: /\.\w+/, token: 'tag'},
+        {regex: /\w+|==|!=|\|/, token: 'builtin'},
+        {regex: /\(/, token: 'variable-2', push: 'expression'},
+        {regex: /\)/, token: 'variable-2', pop: true},
+        {regex: '{', token: 'bracket', push: 'object'},
+        {regex: /\[/, token: 'bracket', push: 'array'}
+    ]
+};
+
+CodeMirror.defineSimpleMode('jq', jqMode);
+
+let sourceEditor, mappingEditor, resultEditor;
+
+const generalOptions = {
+    theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'darcula' : 'default',
+    lineNumbers: true,
+    matchBrackets: true,
+    indentUnit: 4,
+    autocorrect: true,
+    foldGutter: true,
+    lint: true,
+    lintOnChange: true,
+    highlightLines: true,
+    gutters: ["CodeMirror-lint-markers", "CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+    readOnly: false,
+    scrollbarStyle: 'simple',
+    // scrollbarStyle: null,
+}
+const xmlOptions = {
+    mode: "application/xml",
+    alignCDATA: true,
+    htmlMode: false,
+    autoCloseTags: true,
+    matchTags: true,
+};
+const jsonOptions = {
+    mode: "application/json",
+    autoCloseBrackets: true,
+}
+
+const jqOptions = {
+    mode: 'jq',
+    lint: {
+        'getAnnotations': jqValidator,
+        async: true,
+    },
+}
+const readOnlyOptions = {
+    readOnly: 'nocursor',
+    lint: false,
+}
+
+function jqValidator(text, updateLinting) {
+    let errors = [];
+    try {
+        jq.json({}, text)
+    } catch (e) {
+        errors = e.stack.split('jq: ')
+        errors = errors.filter(error => error !== '' && !error.match(/\d+ compile error/g))
+        errors = errors.map(error => {
+            let lineNumber = error.match(/(?<=, line )\d+(?=:$)/m)
+            if (lineNumber) {
+                lineNumber = lineNumber[0]
+            }else{
+                lineNumber = 2
+            }
+            return {
+                from: {
+                    line: lineNumber - 1,
+                    ch: 0,
+                    sticky: null,
+                },
+                to: {
+                    line: lineNumber - 1,
+                    ch: 999,
+                    sticky: null,
+                },
+                message: error,
+                severity: 'error',
+            }
+        })
+    }
+    updateLinting(errors)
+}
+
+function setOptions(editor, options) {
+    Object.entries(options).forEach(option => editor.setOption(option[0], option[1]))
+}
+
+function isXml(string) {
+    return null != string && string.match(/^</)
+}
+
+function setEditorOptions(source, mapping, result) {
+    if (isXml(source) && 'application/xml' !== sourceEditor.getOption('mode')) {
+        setOptions(sourceEditor, {...generalOptions, ...xmlOptions})
+    } else if (!isXml(source) && 'application/json' !== sourceEditor.getOption('mode')) {
+        setOptions(sourceEditor, {...generalOptions, ...jsonOptions})
+    }
+    if (isXml(mapping) && 'application/xml' !== mappingEditor.getOption('mode')) {
+        setOptions(mappingEditor, {...generalOptions, ...xmlOptions})
+        $('#jqRawOption')[0].style.display = 'none'
+    } else if (!isXml(mapping) && 'application/json' !== mappingEditor.getOption('mode') && mapping !== '') {
+        setOptions(mappingEditor, {...generalOptions, ...jqOptions})
+        $('#jqRawOption')[0].style.display = 'inline'
+    }
+    if (isXml(result) && 'application/xml' !== resultEditor.getOption('mode')) {
+        setOptions(resultEditor, {...generalOptions, ...xmlOptions, ...readOnlyOptions})
+    } else if (!isXml(result) && 'application/json' !== resultEditor.getOption('mode')) {
+        setOptions(resultEditor, {...generalOptions, ...jsonOptions, ...readOnlyOptions})
+    }
+}
+
+async function processFields() {
+    let result = '';
+    let source = sourceEditor.getValue()
+    let mapping = mappingEditor.getValue()
+
+    if (!isXml(source) && !isXml(mapping)) {
+        result = await runJq(source, mapping) ?? ''
+    }
+
+    if (isXml(source) && isXml(mapping)) {
+        result = runXsl3(source, mapping) ?? ''
+    }
+
+    setEditorOptions(source, mapping, result)
+
+    let scroll = resultEditor.getScrollInfo()
+    resultEditor.setValue(result)
+    resultEditor.scrollTo(scroll.left, scroll.top)
+}
+
+async function runJq(source, mapping) {
+    try {
+        let result;
+        if($('#jqRaw')[0].checked){
+            result = await jq.promised.raw(JSON.stringify(JSON.parse(source)), mapping, ['-r'])
+        }else{
+            result = JSON.stringify(await jq.promised.json(JSON.parse(source), mapping))
+        }
+        try{
+            result = vkbeautify.json(result)
+        } catch (e) {}
+        return result
+    } catch (e) {
+        return '';
+    }
+}
+
+function runXsl(source, mapping) {
+    try {
+        let xmlParser = new DOMParser();
+        let xslParser = new DOMParser();
+        let xmlSerializer = new XMLSerializer();
+        let processor = new XSLTProcessor();
+        let xslDoc = xslParser.parseFromString(mapping, "application/xml");
+        let xmlDoc = xmlParser.parseFromString(source, "application/xml");
+
+        processor.importStylesheet(xslDoc);
+        let resultDoc = processor.transformToDocument(xmlDoc);
+        let resultXml = xmlSerializer.serializeToString(resultDoc);
+        return '<?xml version="1.0" encoding="UTF-8"?>\n' + vkbeautify.xml(resultXml)
+    } catch (e) {
+        console.log('Exception: ', e);
+        return null;
+    }
+}
+
+function runXsl3(source, mapping) {
+    let saxonPlatform = SaxonJS.getPlatform()
+    let mappingDoc = saxonPlatform.parseXmlFromString(mapping)
+    mappingDoc._saxonBaseUri = "file:///"
+    let compiledMapping = JSON.stringify(SaxonJS.compile(mappingDoc))
+    let compiledMappingUrl = URL.createObjectURL(new Blob([compiledMapping]))
+    let sourceUrl = URL.createObjectURL(new Blob([source]));
+    let resultXml = SaxonJS.transform({
+        stylesheetLocation: compiledMappingUrl,
+        sourceLocation: sourceUrl,
+        destination: 'serialized'
+    }).principalResult
+
+    return vkbeautify.xml(resultXml)
+}
+
+async function upload(event) {
+    let content = await $(event.target).prop('files')[0].text()
+    let newLines = content.match(/\n/g) ?? []
+    if (isXml(content) && newLines.length <= 2) {
+        content = vkbeautify.xml(content)
+    }
+    if (!isXml() && newLines.length <= 2) {
+        try{
+            content = vkbeautify.json(content)
+        }catch (e) {}
+    }
+    event.data.editor.setValue(content)
+}
+
+function updateNetworkStatus (status) {
+    if (status) {
+        $('#networkStatus')[0].style.background = 'green'
+    }else{
+        $('#networkStatus')[0].style.background = 'red'
+    }
+}
+
+$(document).ready(function () {
+    (async function () {
+        let response = await axios.get('https://data-mapper.netlify.app/saxon/2.2/SaxonJS2.js')
+        let saxonJs = response.data.replace(/,xd:function\(n\)\{/, ',xd:function(n){return true;')
+        let saxonJsUrl = URL.createObjectURL(new Blob([saxonJs]))
+        let scriptTag = document.createElement('script')
+        scriptTag.setAttribute('src', saxonJsUrl);
+        document.head.appendChild(scriptTag);
+    })()
+
+    sourceEditor = CodeMirror(document.getElementById('sourceEditor'), generalOptions);
+    sourceEditor.on('change', _.debounce(v => processFields(), debounce));
+    sourceEditor.setSize(null, '100%')
+    $('#sourceUpload').change({editor: sourceEditor}, upload);
+
+
+    mappingEditor = CodeMirror(document.getElementById('mappingEditor'), generalOptions);
+    mappingEditor.on('change', _.debounce(v => processFields(), debounce));
+    mappingEditor.setSize(null, '100%')
+    $('#mappingUpload').change({editor: mappingEditor}, upload);
+
+    resultEditor = CodeMirror(document.getElementById('resultEditor'), {...generalOptions, ...readOnlyOptions});
+    resultEditor.setSize(null, '100%')
+
+    $('#layout').on('change', (event) => {
+        $('.content').get(0).style.gridTemplateAreas = $(event.target).val()
+    })
+    updateNetworkStatus(navigator.onLine)
+    window.addEventListener("online", () => {
+        updateNetworkStatus(true);
+    });
+
+    window.addEventListener("offline", () => {
+        updateNetworkStatus(false);
+    });
+});
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js').then(registration => {
+            // console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+
+module.exports = (env, argv) => {
+    let config = {
+        entry: {
+            main: './src/index.js',
+        },
+        mode: 'development',
+        output: {
+            filename: '[name].[contenthash].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html'
+            }),
+            new WorkboxPlugin.GenerateSW({
+                // these options encourage the ServiceWorkers to get in there fast
+                // and not allow any straggling "old" SWs to hang around
+                clientsClaim: true,
+                skipWaiting: true,
+                maximumFileSizeToCacheInBytes: 99999999999999,
+            }),
+        ],
+        optimization: {
+            runtimeChunk: 'single',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    },
+                },
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.html$/i,
+                    loader: 'html-loader',
+                },
+            ],
+        },
+    }
+
+    if('production' === argv.mode){
+        config.mode = 'production'
+    }
+
+    return config
+};
